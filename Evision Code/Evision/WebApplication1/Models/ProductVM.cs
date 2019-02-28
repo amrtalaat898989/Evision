e6ec316c00a6040e0cdfc5e6b9a8d1a5213d4df5
SaraTMS <sara_talaat91@yahoo.com>
2019-02-28 21:40:31 +0200

    Test

---
+++ Evision Code/Evision/WebApplication1/Models/ProductVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ProductVM
+    {
+        public int Id { get; set; }
+
+
+         [Required]
+        public string Name { get; set; }
+
+       
+        
+        public int Price { get; set; }
+        
+        public HttpPostedFileBase Photo { get; set; }
+
+    }
+}
